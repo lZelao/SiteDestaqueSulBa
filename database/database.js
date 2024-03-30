ec2ee80942cc325578ed6f14e8aa3ff9f08f4d64
Eliseu <esreis123@gmail.com>
2024-03-29 22:09:07 -0300

    mudando usuario banco

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('guiapress','root','Ifba#2018',{
+const connection = new Sequelize('guiapress','eliseu','Ifba#2018',{
     host: 'localhost',
     dialect: 'mysql',
     timezone:"-03:00"
