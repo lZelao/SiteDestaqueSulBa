d4f2eab6b49a2d41cc97ee325fb94a599bc8ace6
Eliseu <esreis123@gmail.com>
2024-05-29 00:55:24 -0300

    anuncie

--- index.js
+++ index.js
@@ -62,6 +62,10 @@ app.get("/",(req, res) =>{
 
 });
 
+app.get('/anuncie', (req, res) => {
+    res.render('Anuncie'); // Renderiza o arquivo Anuncie.ejs
+});
+
 app.get('/quem-somos', (req, res) => {
     res.render('QuemSomos'); // Renderiza o arquivo QuemSomos.ejs
 });
